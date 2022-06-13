b1d66f1198acebb7e8a74a474e77e388827ea584
armandouv <armandouv@fb.com>
2022-06-13 16:08:52 -0700

    Implement login

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -2,14 +2,24 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import com.example.parstagram.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    public final static String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +28,37 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.loginButton.setOnClickListener((view) -> {
+        if (ParseUser.getCurrentUser() != null) {
+            goTimelineActivity();
+            finish();
+            return;
+        }
 
+        binding.loginButton.setOnClickListener((view) -> {
+            Editable username = binding.username.getText();
+            Editable password = binding.password.getText();
+            loginUser(username.toString(), password.toString());
+
+            username.clear();
+            password.clear();
         });
+    }
+
+    private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, (user, e) -> {
+            if (e != null) {
+                Toast.makeText(this, "Couldn't log in", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+            goTimelineActivity();
+            finish();
+        });
+    }
+
+    private void goTimelineActivity() {
+        Intent intent = new Intent(this, TimelineActivity.class);
+        startActivity(intent);
     }
 }
