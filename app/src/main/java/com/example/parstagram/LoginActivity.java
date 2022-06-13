50deef1465ae3100dc07c1430133b582662dd8b6
armandouv <armandouv@fb.com>
2022-06-13 16:09:55 -0700

    Remove unnecessary imports

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -1,20 +1,15 @@
 package com.example.parstagram;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
-import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.parstagram.databinding.ActivityLoginBinding;
-import com.parse.LogInCallback;
-import com.parse.ParseException;
 import com.parse.ParseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
 
