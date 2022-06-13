8ac916031304554e9ff147025cda198f357303ef
armandouv <armandouv@fb.com>
2022-06-13 16:23:07 -0700

    Implement logout

--- app/src/main/java/com/example/parstagram/TimelineActivity.java
+++ app/src/main/java/com/example/parstagram/TimelineActivity.java
@@ -1,14 +1,39 @@
 package com.example.parstagram;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.parstagram.databinding.ActivityTimelineBinding;
+import com.parse.ParseUser;
 
 public class TimelineActivity extends AppCompatActivity {
+    private ActivityTimelineBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
+
+        binding = ActivityTimelineBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.logoutButton.setOnClickListener(v ->
+                ParseUser.logOutInBackground(e -> {
+                    if (e != null) {
+                        Toast.makeText(this, "Couldn't log out", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
+                    goLoginActivity();
+                    finish();
+                }));
+    }
+
+    private void goLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
