ec1b5b21b62bacba5493054e8dddf8d0ce385dd4
armandouv <armandouv@fb.com>
2022-06-17 16:36:47 -0700

    Manage Fragments from TimelineActivity

--- app/src/main/java/com/example/parstagram/posts/TimelineActivity.java
+++ app/src/main/java/com/example/parstagram/posts/TimelineActivity.java
@@ -1,26 +1,16 @@
 package com.example.parstagram.posts;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.fragment.app.Fragment;
 
+import com.example.parstagram.R;
 import com.example.parstagram.databinding.ActivityTimelineBinding;
-import com.example.parstagram.users.LoginActivity;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.parstagram.users.ProfileFragment;
 
 public class TimelineActivity extends AppCompatActivity {
-    private static final String TAG = TimelineActivity.class.getSimpleName();
-    private final List<Post> mPosts = new ArrayList<>();
     private ActivityTimelineBinding mBinding;
-    private PostsAdapter mPostsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,66 +19,23 @@ public class TimelineActivity extends AppCompatActivity {
         mBinding = ActivityTimelineBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
-        mBinding.logoutButton.setOnClickListener(v ->
-                ParseUser.logOutInBackground(e -> {
-                    if (e != null) {
-                        Toast.makeText(this, "Couldn't log out", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show();
-                    goLoginActivity();
-                    finish();
-                }));
-
-        mBinding.composeButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ComposeActivity.class);
-            startActivity(intent);
-        });
-
-        mPostsAdapter = new PostsAdapter(this, mPosts, (view, position) -> {
-            Intent intent = new Intent(TimelineActivity.this, PostDetailsActivity.class);
-            Post post = mPosts.get(position);
-            intent.putExtra(Post.class.getSimpleName(), post);
-            startActivity(intent);
-        });
-
-        mBinding.swipeContainer.setOnRefreshListener(() -> populateHomeTimeline(true));
-
-        mBinding.swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
-
-        mBinding.postsView.setLayoutManager(new LinearLayoutManager(this));
-        mBinding.postsView.setAdapter(mPostsAdapter);
-        populateHomeTimeline(false);
-    }
-
-    private void populateHomeTimeline(boolean isRefreshing) {
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-        query.setLimit(20);
-        query.addDescendingOrder("createdAt");
-
-        query.findInBackground((posts, e) -> {
-            if (e != null) {
-                Log.e(TAG, "Could not query post", e);
-                return;
+        mBinding.bottomNavigation.setOnItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.compose_icon:
+                    fragment = new ComposeFragment();
+                    break;
+                case R.id.profile_icon:
+                    fragment = new ProfileFragment();
+                    break;
+                default:
+                    fragment = new TimelineFragment();
             }
 
-            if (isRefreshing) {
-                mPosts.clear();
-                mBinding.swipeContainer.setRefreshing(false);
-            }
-            mPosts.addAll(posts);
-            mPostsAdapter.notifyDataSetChanged();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
         });
-    }
 
-    private void goLoginActivity() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+        mBinding.bottomNavigation.setSelectedItemId(R.id.home_icon);
     }
 }
